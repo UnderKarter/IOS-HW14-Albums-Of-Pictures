19d7c0dc8291cdca0c0f8586f006192ebcc5d7c6
Semenov Petr <under_karter49@icloud.com>
2022-06-20 15:57:11 +0300

    Initial Commit

---
+++ IOS-HW14-Albums-Of-Pictures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-HW14-Albums-Of-Pictures
+//
+//  Created by temp user on 20.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
